5cad48fea04ea11a6a7565ede1712ca75967bd76
mills9487 <47700808+mills9487@users.noreply.github.com>
2019-11-24 20:27:10 -0600

    More faithful reproduction
    Some things, such as the use of epsilon, had been cut from the original DIRECT algorithm that turned out to be extremely important on functions where the global minimum isn't extremely close to the origin. Also, reversed the 64-to-32-bit change done earlier due to, frankly, pointlessness when epsilon is used.

--- src/ldirect.cr
+++ src/ldirect.cr
@@ -4,15 +4,15 @@ class Hyperrectangle
   property k_tilde
   property center_value
 
-  def initialize(size : Array(Float32), center : Array(Float32))
+  def initialize(size : Array(Float64), center : Array(Float64))
     @size = size
     @center = center
-    @k_tilde = Float32::NAN
-    @center_value = Float32::NAN
+    @k_tilde = Float64::NAN
+    @center_value = Float64::NAN
   end
 
   def radius
-    result = 0_f32
+    result = 0_f64
     size.each do |axis|
       result += axis**2
     end
@@ -36,14 +36,14 @@ class Hyperrectangle
 end
 
 def ccw(p : Hyperrectangle, q : Hyperrectangle, r : Hyperrectangle,
-        value : Proc(Array(Float32), Float32))
+        value : Proc(Array(Float64), Float64))
   (value.call(q.center) - value.call(p.center)) * (r.radius - q.radius) -
     (q.radius - p.radius) * (value.call(r.center) - value.call(q.center))
 end
 
 def get_optimal_hyperrectangles(hyperrectangles : Array(Hyperrectangle),
-                                value : Proc(Array(Float32), Float32),
-                                h : Float32)
+                                value : Proc(Array(Float64), Float64),
+                                h : Float64, current_best : Float64, epsilon : Float64)
   stack = [] of Int32
   hyperrectangles.sort!
   index = 0
@@ -57,44 +57,73 @@ def get_optimal_hyperrectangles(hyperrectangles : Array(Hyperrectangle),
   end
   index = 0
   stack.each do |hr|
-    if index > 0 && hyperrectangles[stack[0]].center_value > hyperrectangles[hr].center_value
+    if index > 0 && hyperrectangles[stack[0]].center_value >= hyperrectangles[hr].center_value
       stack.delete_at(0)
     end
     index += 1
+  end
+  while stack.size >= 2 && hyperrectangles[stack[-1]].radius == hyperrectangles[stack[-2]].radius
+    stack.pop
   end
   index = 0
   stack.each do |optimal|
     if index != stack.size - 1
       hyperrectangles[optimal].k_tilde = (value.call(hyperrectangles[optimal].center) -
-                                          value.call(hyperrectangles[stack[index - 1]].center)) /
+                                          value.call(hyperrectangles[stack[index + 1]].center)) /
                                          (hyperrectangles[optimal].radius -
-                                          hyperrectangles[stack[index - 1]].radius)
+                                          hyperrectangles[stack[index + 1]].radius)
     else
       hyperrectangles[optimal].k_tilde = h
     end
     index += 1
   end
+  index = 0
+  if epsilon != 0
+    stack.each do |optimal|
+      if hyperrectangles[optimal].center_value - (hyperrectangles[optimal].k_tilde * hyperrectangles[optimal].radius) >=
+           (1 + (current_best <= 0 ? 1 : -1) * epsilon) * current_best
+        stack.delete_at(index)
+      else
+        index += 1
+      end
+    end
+  end
   stack
 end
 
-def test(x : Array(Float32)) : Float32
+def test(x : Array(Float64)) : Float64
   # Six-hump camel back function
-  (4_f32 - 2.1_f32*x[0]**2_f32 + x[0]**4_f32/3_f32)*x[0]**2_f32 + x[0]*x[1] + (-4_f32 + 4_f32*x[1]**2_f32)*x[1]**2_f32
+  (4_f64 - 2.1_f64*x[0]**2_f64 + x[0]**4_f64/3_f64)*x[0]**2_f64 + x[0]*x[1] + (-4_f64 + 4_f64*x[1]**2_f64)*x[1]**2_f64
+end
+
+def shubert(x : Array(Float64)) : Float64
+  # Shubert function
+  temp_1 = 0
+  5.times do |i|
+    temp_1 += (i + 1) * Math.cos((i + 2) * x[0] + i + 1)
+  end
+  temp_2 = 0
+  5.times do |i|
+    temp_2 += (i + 1) * Math.cos((i + 2) * x[1] + i + 1)
+  end
+  (temp_1 * temp_2).as(Float64)
 end
 
 puts ("First vanilla DIRECT test results:")
 
 hr_list = [] of Hyperrectangle
-start = Hyperrectangle.new([1_f32, 1_f32], [0_f32, 0_f32])
+start = Hyperrectangle.new([5.12, 5.12], [0_f64, 0_f64])
 hr_list.push(start)
 
 cur = 0_u8
 
-optimals = get_optimal_hyperrectangles(hr_list, ->test(Array(Float32)), 0_f32)
+optimals = get_optimal_hyperrectangles(hr_list, ->shubert(Array(Float64)), 1_f64, -1.0 / 0.0, 0)
 
-50.times do
+current_best = hr_list[optimals[0]].center_value
+
+1000.times do |iteration|
   optimals.each do |index|
-    left, right = hr_list[index].subdivide(cur)
+    left, right = hr_list[index].subdivide((hr_list[index].size.index(hr_list[index].size.max)).as(Int32).to_u8)
     hr_list.push(left)
     hr_list.push(right)
     if cur == 0_u8
@@ -103,9 +132,8 @@ optimals = get_optimal_hyperrectangles(hr_list, ->test(Array(Float32)), 0_f32)
       cur = 0_u8
     end
   end
-  optimals = get_optimal_hyperrectangles(hr_list, ->test(Array(Float32)), 0_f32)
+  optimals = get_optimal_hyperrectangles(hr_list, ->shubert(Array(Float64)), 1_f64, current_best, 0.00001)
+  current_best = hr_list[optimals[0]].center_value
 end
 
-optimals.each do |index|
-  puts hr_list[index].k_tilde
-end
+puts current_best
